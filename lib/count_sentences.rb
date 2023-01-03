ad37428b38de89651065abe19ee662d940d2435d
Lightening Gibson <lighteningibson@gmail.com>
2023-01-02 21:08:21 -0600

    Completed Assignent

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
+#binding.pry
+
+        self.end_with?(".")
+
     
   end
 
   def question?
 
+   self.include?("?")
+
   end
 
   def exclamation?
 
+    self.include?("!")
+
   end
 
   def count_sentences
-
+      delimiters = [".", "!", "?"]
+    self.split(/\.|\?|!/).count
+   
   end
 end
